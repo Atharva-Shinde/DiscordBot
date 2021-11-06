35d9066834de864495be125228316040b5c7fe23
Atharva Shinde <atharvashinde179@gmail.com>
2021-11-07 00:16:46 +0530

    wip

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+
 // view engine is responsible for deciding how we are going to process view which here is ejs
 app.set('view engine', 'ejs')
+
+// this tells express to use the files under public no matter what 
+app.use(express.static("public"))
+
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res)=> {
    const code = `Lorem ipsum dolor 
@@ -11,4 +17,15 @@ app.get('/', (req,res)=> {
     // to send information from sever (i.e server.js) to client i.e (code-display.ejs) we use the second parameter, inside res.render
     res.render('code-display.ejs', {code} )
 })
+
+app.get('/new', (req,res)=>{
+    res.render('new.ejs')
+})
+
+app.post('/save', (req,res)=>{
+    // ...body.value here value is the name we gave inside new.ejs textarea name field 
+    const value = req.body.value
+    console.log(value)
+})
+
 app.listen(3000)
